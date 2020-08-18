41d7ac3ba218977eb8064abc17dfbb5d43df7e5a
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-18 15:02:09 -0500

    transaction

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function(){
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
+    const transaction = document.getElementById("transaction-area");
+    transaction.style.display = "block";
+
 })
