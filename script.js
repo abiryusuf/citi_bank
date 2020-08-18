9e11074e2a34c8ae459c696ba1abe6d50895bd69
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-18 16:30:50 -0500

    CSS

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ loginBtn.addEventListener("click", function(){
     const transaction = document.getElementById("transaction-area");
     transaction.style.display = "block";
 
-})
+});
+
+// const transaction = document.getElementById("transaction-area");
+// var newItem = document.createElement("p");
+// newItem.textContent = "I am Abir";
+// transaction.appendChild(newItem);
