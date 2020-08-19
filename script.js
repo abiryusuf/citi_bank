94d82ef0becc31a82d99ec6cd5b7c5a9ba44d581
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-19 00:18:02 -0500

    deposit button

--- script.js
+++ script.js
@@ -12,8 +12,16 @@ loginBtn.addEventListener("click", function(){
 const depositBtn = document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function(){
     const depositAmount = document.getElementById("depositAmount").value;
+    //convert string to float
     const depositNumber = parseFloat(depositAmount);
-    console.log("deposit");
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+     //convert string to float
+    const currentNumber = parseFloat(currentDeposit);
+    const total = depositNumber + currentNumber;
+    document.getElementById("currentDeposit").innerText = total;
+    document.getElementById("depositAmount").value = "";
+    console.log(total);
 })
 
 // const transaction = document.getElementById("transaction-area");
